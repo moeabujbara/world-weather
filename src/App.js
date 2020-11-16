34954eff36cafa9ec3cbc9a57f6a64dfede649f5
Mahmoud <moe.atlantaga@gmail.com>
2020-11-16 15:51:02 +0200

    Added background color and padding

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { weatherAPi } from "./api.js";
+import { weatherAPi,coordinates } from "./api.js";
 import { useEffect, useState } from "react";
 import Moment from "react-moment";
 import clear from "../src/assets/clear.png";
@@ -16,6 +16,9 @@ function App() {
   let [temp, settemp] = useState([]);
   let [error, setError] = useState([]);
   let [weatherdata, setweatherdata] = useState([]);
+  let [lat, setlat] = useState([]);
+  let [lon, setlon] = useState([]);
+  let [Geographical,setGeographical]=useState([]);
   let [bool, setbool] = useState("false");
 
   const updateInputValue = () => {
@@ -28,10 +31,16 @@ function App() {
 
   const fetch = async (props) => {
     try {
-      let response = await weatherAPi(props);
-      setweatherdata(response.data.list, weatherdata);
-      console.warn("response for data -->", response.data.list);
-      console.warn("Array of weather-->", weatherdata);
+      let response = await coordinates(props);
+      setGeographical(response.data.results[0].geometry, Geographical);
+      console.warn("response for data -->", response.data.results[0].geometry);
+      console.warn("Geographical-->", Geographical.lat);
+      console.warn("Geographical-->", Geographical.lng);
+      let weatherresponse = await weatherAPi(Geographical.lat,Geographical.lng);
+      setweatherdata(weatherresponse.data, weatherdata);
+      console.warn("weatherdata",weatherresponse.data)
+      console.warn("weatherdata",weatherdata);
+
     } catch (error) {
       setError(error.message);
     }
@@ -41,9 +50,12 @@ function App() {
     fetch();
   }, []);
 
+
   return (
     <div className="p-3 mb-2 bg-info text-white text-center">
       <h3 className="mt-3 font-weight-bold">Hello , select your city !</h3>
+      <div className="m-0">
+      </div>
       <div className="ml-4  flex-right">
         <input
           type="text"
@@ -62,41 +74,7 @@ function App() {
         </Button>
 
         <br />
-        <div className="mt-5 d-flex flex-wrap justify-content-around">
-          {weatherdata.slice(0, 5).map((postion, index) => (
-            <Card.Header style="background-color:black" key={index} style={{ width: "12rem" }}>
-              <div>
-                {postion.weather[0].main === "Rain" ? (
-                  <Card.Img variant="top" src={rain} />
-                ) : null}{" "}
-              </div>
-              <div>
-                {postion.weather[0].main === "Clouds" ? (
-                  <Card.Img variant="top" src={cloudy} />
-                ) : null}{" "}
-              </div>
-              <div>
-                {postion.weather[0].main === "Clear" ? (
-                  <Card.Img variant="top" src={clear} />
-                ) : null}{" "}
-              </div>
 
-              <Card.Body>
-                <Card.Title className="font-weight-bold">
-                  {" "}
-                  {postion.weather[0].main}
-                </Card.Title>
-                <Card.Text>
-                  <Moment format="yy-MM-d">
-                    <h6>{postion.dt_txt} </h6>
-                  </Moment>
-                  <h6>Max: {postion.main.temp_max}</h6>
-                  <h6>Min: {postion.main.temp_min}</h6>
-                </Card.Text>
-              </Card.Body>
-            </Card.Header>
-          ))}
-        </div>
       </div>
     </div>
   );
