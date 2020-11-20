66e8010aab05c3f8a64d77ff4b15394e4886c16f
Mahmoud <moe.atlantaga@gmail.com>
2020-11-20 23:37:56 +0200

    changed a function name

--- src/api.js
+++ src/api.js
@@ -3,13 +3,15 @@ export const weatherAPi = (lat,lon) => {
     return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,alerts}&units=imperial&appid=9ef82b487aa3f82c87969a56bd3ae31c`);
     
   };
-  export const coordinates = (props) => {
+  export const coordinatesf = (props) => {
     return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${props}&key=6ab50404c44048db898036968cfedc2f`);
     
   };
+
+
  
   
-export default weatherAPi;  
+  
 
 
 
