12b450dddab48371841cc163ade2a299c20d39e2
Mahmoud <moe.atlantaga@gmail.com>
2020-11-21 16:06:39 +0200

    change api key

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 export const weatherAPi = (lat,lon) => {
-    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,alerts}&units=imperial&appid=9ef82b487aa3f82c87969a56bd3ae31c`);
+    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={current,minutely,alerts}&units=imperial&appid=3e6c02adede40052e8a024a75e679447`);
     
   };
   export const coordinatesf = (props) => {
-    return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${props}&key=6ab50404c44048db898036968cfedc2f`);
+    return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${props}&key=f4b614147915465385a24ed744451f58`);
     
   };
 
