760204e7cf3acadd4647bbb3dbbd0a132c61fd41
Mahmoud <moe.atlantaga@gmail.com>
2020-11-25 23:21:34 +0200

    conect the app page with auto complete page and send the data detween them

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { weatherAPi} from "./api.js";
-import {coordinatesf} from "./api.js";
-import { useEffect, useState ,useRef } from "react";
+import { weatherAPi } from "./api.js";
+import { coordinatesf } from "./api.js";
+import { autocompleteApi } from "./api.js";
+import { useEffect, useState, useRef } from "react";
 import Moment from "react-moment";
 import clear from "../src/assets/clear.png";
 import cloudy from "../src/assets/cloudy.png";
 import rain from "../src/assets/rain.png";
 import Weatherforyourlocation from "./weatherforyourlocation";
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
 import { lang } from "moment";
-import MapContainer from './viewmap.js';
+import MapContainer from "./viewmap.js";
 import snow from "../src/assets/snowy.png";
+import Autocomplete from "./autocomplete.js";
 
-let element;
+
 function App(props) {
   let [temp, settemp] = useState([]);
   let [error, setError] = useState([]);
@@ -30,15 +32,19 @@ function App(props) {
   let [key, setkey] = useState([]);
   let [check, setcheck] = useState(false);
   let [coordinates, setcoordinates] = useState([]);
-  let [center,setcenter]=useState([]);
-  
-  
+  let [center, setcenter] = useState([]);
+  let [input, setInput] = useState([]);
+  let [exported, setexported] = useState([]);
+ 
+
+ 
   const updateInputValue = () => {
+    console.warn("exported data is goin to be", exported);
     setcheck(true);
-    var country = document.getElementById("city").value;
-    fetch(country);
+    fetch(exported);
     setbool(true);
   };
+
   const toggleSelected = () => {
     if (selected) {
       weatherdata.slice(0, 5).forEach((element, i) => {
@@ -62,65 +68,65 @@ function App(props) {
       });
     }
   };
-   fetch = async (props) => {
-       try{
+  fetch = async (props) => {
+    try {
       let response = await coordinatesf(props);
-      setcenter(response.data.results[0].geometry,center);
+      setcenter(response.data.results[0].geometry, center);
       let weatherresponse = await weatherAPi(
         response.data.results[0].geometry.lat,
         response.data.results[0].geometry.lng
       );
       setweatherdata(weatherresponse.data.daily, weatherdata);
-      console.warn("orginal",weatherresponse.data)
-      console.warn("weatherdatadat",weatherdata)}
-      catch (error) {
-        setError(error.message);
-      }
+    } catch (error) {
+      setError(error.message);
+    }
   };
   useEffect(() => {
     fetch();
   }, []);
 
+  const handleChange=(newvalue)=>{
+    setexported(newvalue);
+  }
+ 
   return (
     <div className=" mb-2 bg-info text-white text-center position:relative overflow-auto">
       <h3 className="mt-3 font-weight-bold">Hello , select your city !</h3>
       <div className="flex-right">
-        <input
-          type="text"
-          id="city"
-          className="px-10 py-10 shadow-lg bg-grey rounded"
-          placeholder="Search"
-        />
-        <br />
-        <Button
-          onClick={updateInputValue}
+        <Autocomplete value={props.value} onChange={handleChange}></Autocomplete>
+        <br/>
+        <button
           className="mt-2 btn btn-warning rounded"
           size="md"
+          onClick={updateInputValue}
         >
           Check Weather
-        </Button>
-     
-       {check ?(
-         <div className="d-flex flex-column flex-md-row justify-content-between">
-           <div id="f">
-           <MapContainer  data={center}> </MapContainer>
-           </div>
-         <div id="toggle-container" className="mt-5 mt-md-0" onClick={toggleSelected}>
-         <div className={`dialog-button mt-0 ${selected ? "" : "disabled"}`}>
-           {selected ? "F" : "C"}
-         </div>
-       </div>
-       </div>
-       ):
-        (null)
-      }
-       <br />
-          {check ? (
+        </button>
+        
+        {check ? (
+          <div className="d-flex flex-column flex-md-row justify-content-between">
+            <div id="f">
+              <MapContainer data={center}> </MapContainer>
+            </div>
+            <div
+              id="toggle-container"
+              className="mt-5 mt-md-0"
+              onClick={toggleSelected}
+            >
+              <div
+                className={`dialog-button mt-0 ${selected ? "" : "disabled"}`}
+              >
+                {selected ? "F" : "C"}
+              </div>
+            </div>
+          </div>
+        ) : null}
+        <br />
+        {check ? (
           <div
             className="mt-5 d-flex flex-wrap justify-content-around flex-row mx-auto"
             id="main"
           >
-            
             {weatherdata.slice(0, 5).map((postion, index) => (
               <Card.Header
                 id={"weather-card"}
@@ -130,11 +136,11 @@ function App(props) {
                 className="mx-auto mb-3"
               >
                 <div>
-                {postion.weather[0].main =="Snow" ? (
-                <Card.Img variant="top" src={snow} />
-              ) : null}
-              </div>
-              <div>
+                  {postion.weather[0].main == "Snow" ? (
+                    <Card.Img variant="top" src={snow} />
+                  ) : null}
+                </div>
+                <div>
                   {postion.weather[0].main === "Rain" ? (
                     <Card.Img variant="top" src={rain} />
                   ) : null}{" "}
@@ -156,7 +162,9 @@ function App(props) {
                   </Card.Title>
                   <Card.Text>
                     <h6>
-                  <Moment unix format="D-MM-yyyy">{postion.dt}</Moment>
+                      <Moment unix format="D-MM-yyyy">
+                        {postion.dt}
+                      </Moment>
                     </h6>
                     <br />
                     <h6>
@@ -171,7 +179,6 @@ function App(props) {
                 </Card.Body>
               </Card.Header>
             ))}
-
           </div>
         ) : (
           <Weatherforyourlocation />
